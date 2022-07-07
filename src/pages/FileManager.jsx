7adab7f454e46675c542064efc5f151b1eb22bd0
Jason Johnston <killean@shaw.ca>
2022-07-07 16:51:13 -0500

    working on context

--- src/pages/FileManager.jsx
+++ src/pages/FileManager.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PCSContext from '../components/PCSContext';
 import Button from '../components/Button';
 
 export default function FileManager() {
+    let PCSD = useContext(PCSContext);
+
+    function NewCharacter() {
+        let newFiles = PCSD.files;
+        newFiles.push({
+            title: "New Display Title",
+            loaded: false,
+            saved: false,
+            data: {}
+        });
+
+        PCSD.setFiles(newFiles);
+
+        console.log(PCSD.files);
+    }
+
     function RenderCharacters() {
-        return ["char 1", "char 2", "char 3"].map((char, index) => {
+        if (PCSD.files.length == 0)
+            return (<p>No characters have been loaded or created...</p>);
+
+        return PCSD.files.map((files, index) => {
             return (
                 <div key={`char${index}`} className="border border-black p-1 flex flex-row items-center space-x-2">
-                    <div className="flex-grow">{char}</div>
-                    <div className="">Saved</div>
+                    <div className="flex-grow">{files.title}</div>
+                    <div className="">{(files.saved)?"Saved":"Unsaved"}</div>
                     <div className="flex flex-row space-x-1">
-                        <Button color="green" className="bi-save" />
-                        <Button color="red" className="bi-trash" />
+                        {(files.loaded)?<Button color="disabled" className="bi-play-circle" />:<Button color="purple" className="bi-play-circle" onClick={()=>console.log(`Activating ${files.name}`)} />}
+                        <Button color="green" className="bi-save" onClick={()=>console.log(`Saving ${files.title}`)} />
+                        <Button color="red" className="bi-trash" onClick={()=>console.log(`Removing ${files.title}`)} />
                     </div>
                 </div>
             );
@@ -21,7 +42,7 @@ export default function FileManager() {
         <>
             <h1>File Manager</h1>
             <div className="flex justify-evenly m-3">
-                <Button color="blue">New Character</Button>
+                <Button color="blue" onClick={NewCharacter}>New Character</Button>
                 <Button color="yellow">Load Character</Button>
             </div>
             <h2>Characters</h2>
