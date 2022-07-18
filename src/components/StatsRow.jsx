6e34c3de76237efbffaf550e6c430470698a43ca
Jason Johnston <thatwebguru@gmail.com>
2022-07-17 19:31:21 -0500

    Finished stats page and controls

--- src/components/StatsRow.jsx
+++ src/components/StatsRow.jsx
@@ -10,7 +10,6 @@ export default function StatsRow({title, id, value, className, onChange, ...prop
     if (_.isNil(className)) className = "";
     if (_.isNil(onChange)) console.warn("In order to get the text, you need to assign something to onChange...");
 
-    let [ open, setOpen ] = useState(false);
     let [ rowValue, setRowValue ] = useState(value);
     let [ rowId, setRowId ] = useState(id);
 
@@ -22,25 +21,56 @@ export default function StatsRow({title, id, value, className, onChange, ...prop
     function ChangeValue(e, index) {
         let inputValue = e.target.value;
         let newValue = [...rowValue];
-        newValue[index] = inputValue;
+        newValue[index] = parseInt(inputValue) | 0;
 
         setRowValue(newValue);
         if (!_.isNil(onChange)) onChange(newValue);
     }
 
+    function OpenClose(e) {
+        let container = e.target;
+        let sibling = container.nextSibling;
+
+        if (container.classList.contains("rounded-b-md")) {
+            container.classList.remove("rounded-b-md");
+            sibling.classList.remove("hidden");
+            sibling.classList.add("flex");
+        }
+        else {
+            container.classList.add("rounded-b-md");
+            sibling.classList.add("hidden");
+            sibling.classList.remove("flex");
+        }
+    }
+
+    let stattotal = rowValue[0] + rowValue[1] + rowValue[2] + rowValue[3];
+    let modifier = Math.floor((stattotal - 10) / 2);
+
     return (
-        <div className="flex flex-row space-x-1 bg-amber-300 p-1 cursor-pointer">
-            {(open)?(
-                <>
-                    <p>Blah</p>
-                </>
-            ):(
-                <>
-                    <div className="flex-grow font-bold">Strength</div>
-                    <div className="border">Total</div>
-                    <div className="border rounded-full">Mod</div>
-                </>
-            )}
+        <div className={className}>
+            <div className="statsrow-top rounded-b-md" onClick={OpenClose}>
+                <div className="statsrow-title">{title}</div>
+                <div className="statsrow-total">{stattotal}</div>
+                <div className="statsrow-mod">{modifier}</div>
+            </div>
+            <div className="statsrow-bottom hidden">
+                <div className="statsrow-cont">
+                    <label htmlFor={`${rowId}-base`} className="statsrow-label">Base</label>
+                    <input type="number" id={`${rowId}-base`} name={`${rowId}-base`} value={rowValue[0]} className="statsrow-num" onChange={(e)=>ChangeValue(e,0)} />
+                </div>
+                <div className="statsrow-cont">
+                    <label htmlFor={`${rowId}-enh`} className="statsrow-label">Enhance</label>
+                    <input type="number" id={`${rowId}-enh`} name={`${rowId}-enh`} value={rowValue[1]} className="statsrow-num" onChange={(e)=>ChangeValue(e,1)} />
+                </div>
+                <div className="statsrow-cont">
+                    <label htmlFor={`${rowId}-misc`} className="statsrow-label">Misc</label>
+                    <input type="number" id={`${rowId}-misc`} name={`${rowId}-misc`} value={rowValue[2]} className="statsrow-num" onChange={(e)=>ChangeValue(e,2)} />
+                </div>
+                <div className="statsrow-cont">
+                    <label htmlFor={`${rowId}-temp`} className="statsrow-label">Temp</label>
+                    <input type="number" id={`${rowId}-temp`} name={`${rowId}-temp`} value={rowValue[3]} className="statsrow-num" onChange={(e)=>ChangeValue(e,3)} />
+                </div>
+            </div>
         </div>
     );
 }
