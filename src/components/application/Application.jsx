39d86f133bfb416cdb87e2756b3f87e426be23a5
Jason Johnston <killean@shaw.ca>
2024-02-07 15:48:00 -0600

    Updated menu structure code. More simplistic.

--- src/components/application/Application.jsx
+++ src/components/application/Application.jsx
@@ -5,64 +5,10 @@ import { themeChange } from 'theme-change';
 import Button from '../Button';
 import PCSContext from './PCSContext';
 import IconicIcon from '../../images/iconic';
-import FileManager from '../../pages/FileManager';
-import Details from '../../pages/Details';
-import Stats from '../../pages/Stats';
-import Classes from '../../pages/Classes';
-import Health from '../../pages/Health';
-import Saves from '../../pages/Saves';
-import Offensive from '../../pages/Offensive';
-import Defensive from '../../pages/Defensive';
-import Miscellaneous from '../../pages/Miscellaneous';
-import Weapons from '../../pages/Weapons';
-import Armors from '../../pages/Armors';
-import Skills from '../../pages/Skills';
-import ExpWealth from '../../pages/ExpWealth';
-import Abilities from '../../pages/Abilities';
-import Equipment from '../../pages/Equipment';
-import Spellbook from '../../pages/Spellbook';
-import Notes from '../../pages/Notes';
-import Tools from '../../pages/Tools';
-import Components from '@/pages/tests/Components';
-import Typography from '@/pages/tests/Typography';
-
-const CHAR_PAGES = [
-    {url:"details",icon:"bi-journal-text",name:"Character Details",component:(<Details />)},
-    {url:"ability-scores",icon:"bi-person-lines-fill",name:"Ability Scores",component:(<Stats />)},
-    {url:"classes",icon:"bi-briefcase",name:"Classes",component:(<Classes />)},
-    {url:"health",icon:"bi-plus-circle",name:"Health",component:(<Health />)},
-    {url:"saving-throws",icon:"bi-hospital",name:"Saving Throws",component:(<Saves />)},
-    {url:"offense",icon:"bi-lightning",name:"Offensive Stats",component:(<Offensive />)},
-    {url:"defense",icon:"bi-shield",name:"Defensive Stats",component:(<Defensive />)},
-    {url:"misc-stats",icon:"bi-sliders",name:"Miscellaneous Stats",component:(<Miscellaneous />)},
-    {url:"weapons",icon:"bi-tornado",name:"Weapons",component:(<Weapons />)},
-    {url:"armor",icon:"bi-robot",name:"Armors",component:(<Armors />)},
-    {url:"skills",icon:"bi-plus-slash-minus",name:"Skills",component:(<Skills />)},
-    {url:"exp-wealth",icon:"bi-coin",name:"EXP and Wealth",component:(<ExpWealth />)},
-    {url:"feats-abilities",icon:"bi-cloud-haze2",name:"Feats and Abilities",component:(<Abilities />)},
-    {url:"equipment",icon:"bi-check2-square",name:"Equipment",component:(<Equipment />)},
-    {url:"spellbook",icon:"bi-book",name:"Spellbook",component:(<Spellbook />)},
-    {url:"notes",icon:"bi-body-text",name:"Notes",component:(<Notes />)},
-];
+import { MAIN_MENU } from '@/scripts/Menu';
 
 export default function Application() {
     let PCSD = useContext(PCSContext);
-    let GoogleScopes = [
-        encodeURIComponent("https://www.googleapis.com/auth/userinfo.email"),
-        encodeURIComponent("https://www.googleapis.com/auth/userinfo.profile"),
-        "openid",
-        encodeURIComponent("https://www.googleapis.com/auth/drive.file")
-    ];
-    let GoogleLink = "https://accounts.google.com/o/oauth2/v2/auth?"
-        + "client_id=850992449367-pmpcqbqueupfpvi34vt6se4mtmoo6ooq.apps.googleusercontent.com&"
-        + "redirect_uri=" + encodeURIComponent("http://localhost:5173/") + "&"
-        + "response_type=token&"
-        + "scope=" + GoogleScopes.join(" ");
-    
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has("code") || urlParams.has("access_token")) {
-        console.log("DATA", urlParams);
-    }
 
     useEffect(() => themeChange(false), []);
 
@@ -90,18 +36,24 @@ export default function Application() {
 
     function RenderMenu() {
         let menu = [];
-        menu.push(<Link key="menu0" className="bi-hdd" to="/" onClick={HideMobileNav}> File Manager</Link>);
+        let isActive = (PCSD.getLoadedIndex() != -1);
 
-        if (PCSD.getLoadedIndex() != -1) {
-            CHAR_PAGES.forEach((item, i) => {
-                menu.push(<Link key={`menu${i+1}`} className={item.icon} to={item.url} onClick={HideMobileNav}> {item.name}</Link>);
-            });
-        }
+        return MAIN_MENU.map(menuItem => ((!menuItem.onactive || (menuItem.onactive && isActive)) && !menuItem.hidden) ? (
+                <Link key={`menu-${menuItem.url}`} className={menuItem.icon} to={menuItem.url} onClick={HideMobileNav}> {menuItem.name}</Link>
+            ):null
+        );
+        // menu.push(<Link key="menu0" className="bi-hdd" to="/" onClick={HideMobileNav}> File Manager</Link>);
 
-        menu.push(<Link key={`menu${menu.length + 1}`} className="bi-tools" to="/tools" onClick={HideMobileNav}> Tools</Link>);
-        menu.push(<Link key={`menu${menu.length + 2}`} className="bi-code" to="/components" onClick={HideMobileNav}> Components</Link>);
-        menu.push(<Link key={`menu${menu.length + 3}`} className="bi-fonts" to="/typography" onClick={HideMobileNav}> Typography</Link>);
-        menu.push(<a key={`menu${menu.length + 4}`} className="bi-google" href={GoogleLink} onClick={HideMobileNav}> Google Login</a>);
+        // if (PCSD.getLoadedIndex() != -1) {
+        //     CHAR_PAGES.forEach((item, i) => {
+        //         menu.push(<Link key={`menu${i+1}`} className={item.icon} to={item.url} onClick={HideMobileNav}> {item.name}</Link>);
+        //     });
+        // }
+
+        // menu.push(<Link key={`menu${menu.length + 1}`} className="bi-tools" to="/tools" onClick={HideMobileNav}> Tools</Link>);
+        // menu.push(<Link key={`menu${menu.length + 2}`} className="bi-code" to="/components" onClick={HideMobileNav}> Components</Link>);
+        // menu.push(<Link key={`menu${menu.length + 3}`} className="bi-fonts" to="/typography" onClick={HideMobileNav}> Typography</Link>);
+        // menu.push(<a key={`menu${menu.length + 4}`} className="bi-google" href={GoogleLink} onClick={HideMobileNav}> Google Login</a>);
 
         return menu;
     }
@@ -126,13 +78,20 @@ export default function Application() {
                 </header>
                 <main className="flex flex-col flex-grow">
                     <Routes>
-                        <Route index element={<FileManager />} />
+                        {MAIN_MENU.map(menuItem => {
+                            let addIndex = (menuItem.index) ? { index: true } : {};
+
+                            return (
+                                <Route key={`route-${menuItem.url}`} {...addIndex} path={menuItem.url} element={menuItem.component} />
+                            );
+                        })}
+                        {/* <Route index element={<FileManager />} />
                         <Route path="tools/*" element={<Tools />} />
                         <Route path="components" element={<Components />} />
                         <Route path="typography" element={<Typography />} />
                         {RenderRoutes()}
                         <Route path="404" element={<p>Page not found</p>} />
-                        <Route path="*" element={<Navigate to="/404" />} />
+                        <Route path="*" element={<Navigate to="/404" />} /> */}
                     </Routes>
                 </main>
             </div>
